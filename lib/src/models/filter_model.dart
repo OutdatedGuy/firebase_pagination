f5d17f36d60ad0c32326c65188b8e581bc4f9dfb
delwar36 <delwarh543@gmail.com>
2024-11-11 14:55:18 +0600

    Good to go

--- lib/src/models/filter_model.dart
+++ lib/src/models/filter_model.dart
@@ -1,10 +1,11 @@
+/// A class for creating a search filter model in [FirestorePagination]
 class FilterModel {
+  /// Creates a [FilterModel] object
+  FilterModel({required this.fieldName, required this.searchValue});
+
   /// The name of the field in collection to be searched.
   final String fieldName;
 
   /// The substring that is intented to search in [fieldName] value.
   final String searchValue;
-
-  /// Creates a object that contains the properties of the [FilterModel] model.
-  FilterModel({required this.fieldName, required this.searchValue});
 }
