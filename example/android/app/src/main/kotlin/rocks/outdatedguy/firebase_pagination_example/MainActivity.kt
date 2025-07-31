106dffcd7821872f391239d1a277d7b724ecc83b
OutdatedGuy <everythingoutdated@gmail.com>
2025-07-31 10:04:49 +0530

    chore: updated example template to Flutter 3.32 (#74)

--- example/android/app/src/main/kotlin/rocks/outdatedguy/firebase_pagination_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/rocks/outdatedguy/firebase_pagination_example/MainActivity.kt
@@ -2,4 +2,4 @@ package rocks.outdatedguy.firebase_pagination_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
